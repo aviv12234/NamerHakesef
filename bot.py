bbabe91b7db80c2992523953da894de789bb798c
aviv12234 <58222380+aviv12234@users.noreply.github.com>
2022-04-08 13:31:53 +0300

    made the attack
    the bot pushed with 25 marines with a little bit of micro

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ from sc2.main import run_game
 from sc2.data import Race, Difficulty
 from sc2.bot_ai import BotAI
 from sc2.ids.unit_typeid import UnitTypeId
+from sc2.ids.ability_id import AbilityId
+
 
 
 class WorkerRushBot(BotAI):
@@ -19,6 +21,9 @@ class CurrentBot(BotAI):
         await self.build_workers()
         await self.build_depots()
         await self.build_barracks()
+        await self.build_marines()
+        await self.micro()
+        await self.depot_control()
 
     async def build_workers(self):
         command_center = self.townhalls.ready.random
@@ -55,10 +60,57 @@ class CurrentBot(BotAI):
         ):
 
             await self.build(UnitTypeId.BARRACKS, near=barrack_positions)
+
+
+    async def build_marines(self):
+
+        if( 
+            self.can_afford(UnitTypeId.MARINE)
+            and self.supply_left > 0
+        ):
+            for barrack in self.structures(UnitTypeId.BARRACKS).idle:
+                barrack.train(UnitTypeId.MARINE)
+
+    async def micro(self):
+
+        marinecount = self.units(UnitTypeId.MARINE).amount
+        marines = self.units(UnitTypeId.MARINE).ready
+
+        if(marinecount > 25):
+
+            for marine in marines:
+                
+                if marine.weapon_cooldown == 0:
+                    marine.attack(self.enemy_start_locations[0])
+                else:
+                    marine.move(self.start_location)
+
+
+    async def depot_control(self):
+
+        for depo in self.structures(UnitTypeId.SUPPLYDEPOT).ready:
+            for unit in self.enemy_units:
+                if unit.distance_to(depo) < 15:
+                    break
+            else:
+                depo(AbilityId.MORPH_SUPPLYDEPOT_LOWER)
+
+
+        for depo in self.structures(UnitTypeId.SUPPLYDEPOTLOWERED).ready:
+            for unit in self.enemy_units:
+                if unit.distance_to(depo) < 10:
+                    depo(AbilityId.MORPH_SUPPLYDEPOT_RAISE)
+                    break
+
+
+
+
+
+            
         
 
 
-run_game(maps.get("HardwireAIE"), [
+run_game(maps.get("GlitteringAshesAIE"), [
     Bot(Race.Terran, CurrentBot()),
-    Computer(Race.Zerg, Difficulty.Medium)
+    Computer(Race.Zerg, Difficulty.VeryHard)
 ], realtime=False)
